cacb396f32dbad26b07d983f7d98265f5a7cc0ae
Daniel Mazurek <omrfg.2@gmail.com>
2019-05-11 22:56:49 +0200

    Implemented adding/removing user for/from project

--- Releaseasy/Controllers/ProjectController.cs
+++ Releaseasy/Controllers/ProjectController.cs
@@ -14,70 +14,90 @@ namespace Releaseasy.Controllers
     [ApiController]
     public class ProjectController : ControllerBase
     {
+        private readonly ReleaseasyContext context;
+
+        public ProjectController(ReleaseasyContext context)
+        {
+            this.context = context;
+        }
+
+
         // GET: api/Project
         [HttpGet]
         public ActionResult<IEnumerable<Project>> Get()
         {
-            using (var context = new ReleaseasyContext())
-            {
-                return context.Projects.ToArray();
-            }
+            return context.Projects.ToArray();
+         
         }
 
         // GET: api/Project/5
         [HttpGet("{id}")]
         public ActionResult<Project> Get(int id)
         {
-            using (var context = new ReleaseasyContext())
-            {
-                return context.Projects.Find(id);
-            }
+           return context.Projects.Find(id);
         }
 
         // POST: api/Project
         [HttpPost]
         public void Post([FromBody] Project value)
         {
-            using (var context = new ReleaseasyContext())
+            
+            try
             {
-                try
-                {
-                    context.Add(value);
-                    context.SaveChanges();
-                }
-                catch (ValidationException ex)
-                {
-                    throw;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
+                context.Add(value);
+                context.SaveChanges();
             }
+            catch (ValidationException ex)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
         }
         [HttpPost("AddUser")]
-        public void AddUser([FromBody] AddUserParameters inc )
-        {     
-            using (var context = new ReleaseasyContext())
+        public void AddUser([FromBody] UserProjectPair inc )
+        {               
+            User user = context.Users.Where(u => u.Id == inc.UserId).Include(u => u.Projects).Single();
+            Project project = context.Projects.Where(p => p.Id == inc.ProjectId).Include(u => u.Users).Single();
+
+            if (user != null && project != null)
             {
-                User user = context.Users.Where(u => u.Id == inc.UserId).Include(u => u.Projects).Single();
-                Project project = context.Projects.Where(p => p.Id == inc.ProjectId).Include(u => u.Users).Single();
-
-                if (user != null && project != null)
+                var projectUser = new ProjectUser
                 {
-                    var projectUser = new ProjectUser
-                    {
-                        UserId = user.Id,
-                        ProjectId = project.Id
-                    };
+                    UserId = user.Id,
+                    ProjectId = project.Id
+                };
 
-                    project.Users.Add(projectUser);
-                    user.Projects.Add(projectUser);
-                    context.SaveChanges();
+                project.Users.Add(projectUser);
+                user.Projects.Add(projectUser);
+                context.SaveChanges();
+            }
+        }
+
+        [HttpPost("RemoveUser")]
+        public void RemoveUser([FromBody] UserProjectPair inc)
+        {
+            User user = context.Users.Where(u => u.Id == inc.UserId).Include(u => u.Projects).Single();
+
+            if (user != null)
+            {
+                foreach (var connection in user.Projects)
+                {
+                    if (inc.ProjectId == connection.ProjectId)
+                    {
+                        Project projectToDeleteUserFrom = context.Projects.Where(p => p.Id == inc.ProjectId).Include(p => p.Users).Single();
+                        user.Projects.Remove(connection);
+                        projectToDeleteUserFrom.Users.Remove(connection);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Selected user is not a member of selected project.");
+                    }
                 }
             }
-
-
         }
 
         // PUT: api/Project/5
@@ -85,36 +105,30 @@ namespace Releaseasy.Controllers
         public void Put(int id, [FromBody] Project value)
         {
             Project project;
-
-            using (var context = new ReleaseasyContext())
-            {
-                project = context.Projects.Find(id);
             
-                if (project != null)
+            project = context.Projects.Find(id);
+            
+            if (project != null)
+            {
+                if (value.Description != null)
                 {
-                    if (value.Description != null)
-                    {
-                        project.Description = value.Description;
-                    }
-                    if (value.Name != null)
-                    {
-                        project.Name = value.Name;
-                    }
-                    if (value.StartTime != null)
-                    {
-                        project.StartTime = value.StartTime;
-                    }
-                    if (value.EndTime != null)
-                    {
-                        project.EndTime = value.EndTime;
-                    }
-
-
-                    context.Entry(project).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
-
-                    context.SaveChanges();
+                    project.Description = value.Description;
                 }
-              
+                if (value.Name != null)
+                {
+                    project.Name = value.Name;
+                }
+                if (value.StartTime != null)
+                {
+                    project.StartTime = value.StartTime;
+                }
+                if (value.EndTime != null)
+                {
+                    project.EndTime = value.EndTime;
+                }
+
+                context.Entry(project).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
             }
         }
 
@@ -123,22 +137,17 @@ namespace Releaseasy.Controllers
         public void Delete(int id)
         {
             Project project;
+            
+            project = context.Projects.Find(id);
 
-            using (var context = new ReleaseasyContext())
+            if (project != null)
             {
-                project = context.Projects.Find(id);
-
-                if (project != null)
-                {
-                    context.Remove(project);  
-                    context.SaveChanges();
-                }
-
+                context.Remove(project);  
+                context.SaveChanges();
             }
-
         }
 
-        public class AddUserParameters
+        public class UserProjectPair
         {
             public int UserId { get; set; }
             public int ProjectId { get; set; }
