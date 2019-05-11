808f4692da92e25e2bb5c7a051ecb3fa991d6254
Daniel Mazurek <omrfg.2@gmail.com>
2019-05-11 21:24:52 +0200

    Appsettings and ReleaseeasyContext updated

--- Releaseasy/Model/ReleaseasyContext.cs
+++ Releaseasy/Model/ReleaseasyContext.cs
@@ -5,6 +5,16 @@ namespace Releaseasy
 {
     public class ReleaseasyContext : DbContext
     {
+        public ReleaseasyContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public ReleaseasyContext()
+        {
+
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Task> Tasks { get; set; }
